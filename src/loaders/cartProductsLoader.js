c469a52cff0b1d6ad30ad5f160c0d81ff416f46b
jishan1019 <imranislamjishan200@gmail.com>
2023-05-16 18:45:55 +0600

    card data reviiew pblm solve

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,11 +1,24 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
+
+    const storedCart = getShoppingCart();
+    const ids = Object.keys(storedCart)
+
+
+
+
+    const loadedProducts = await fetch('http://localhost:5000/productsByIds', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(ids)
+    });
     const products = await loadedProducts.json();
 
     // if cart data is in database, you have to use async await
-    const storedCart = getShoppingCart();
+    
 
     const savedCart = [];
 
